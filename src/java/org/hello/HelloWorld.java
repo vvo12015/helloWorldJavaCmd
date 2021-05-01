9c689ea36a259a53cf2d882002d0b6186931e9b1
Vrakin Valentin <vvo12015@gmail.com>
2021-05-01 08:02:37 +0300

    added work of files

--- src/java/org/hello/HelloWorld.java
+++ src/java/org/hello/HelloWorld.java
@@ -1,9 +1,34 @@
 package org.hello;
 
+import java.util.Scanner;
+
 public class HelloWorld{
 	
 	public static void main(String[] args){
-		System.out.println("HelloWorld!!!");
+		try{
+			System.out.println("program code");
+			var sc = new Scanner(System.in);
+			var enter = sc.nextInt();
+			sc.close();
+			var helloWorld = new HelloWorld();
+			if (enter == 1) throw helloWorld.new BasketException("BasketException");
+			else throw helloWorld.new OrderException("OrderException");
+		}catch(BasketException | OrderException e){
+			System.out.println(e.getMessage());
+		}
+		System.out.println("next program code");
 	}
 	
+	class BasketException extends Exception{
+		BasketException(String message){
+			super(message);
+		}
+	}
+	
+	class OrderException extends Exception{
+		OrderException(String message){
+			super(message);
+		}
+	}
+		
 }
